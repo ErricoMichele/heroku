0003e8c1583c3d37dc5f075267cad60bad05ca57
ErricoMichele <81233045+ErricoMichele@users.noreply.github.com>
2022-01-15 01:20:42 +0100

    Delete .history directory

--- .history/index_20220115005450.js
+++
@@ -1,9 +0,0 @@
-const { Client, Intents } = require('discord.js');
-const client  = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-const config = require('./config.json')
-
-client.on('ready', () => {
-    console.log('the client on')
-})
-
-client.login(config.token)
